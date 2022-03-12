bc5bba0a50435e7e43bcc8eba8a3d21ad9cdf362
stephanosterburg <stephanosterburg@me.com>
2022-03-12 12:35:50 +0000

    Initial commit

---
+++ src/graphedge.cpp
@@ -0,0 +1,12 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphEdge::GraphEdge(int id) { _id = id; }
+
+void GraphEdge::SetChildNode(GraphNode *childNode) { _childNode = childNode; }
+
+void GraphEdge::SetParentNode(GraphNode *parentNode) {
+  _parentNode = parentNode;
+}
+
+void GraphEdge::AddToken(std::string token) { _keywords.push_back(token); }
---
+++ src/graphnode.cpp
@@ -0,0 +1,45 @@
+#include "graphnode.h"
+#include "graphedge.h"
+
+GraphNode::GraphNode(int id) { _id = id; }
+
+GraphNode::~GraphNode() {
+  //// STUDENT CODE
+  ////
+
+  ////
+  //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token) { _answers.push_back(token); }
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
+  _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(unique_ptr<GraphEdge> edge) {
+  _childEdges.push_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot) {
+  _chatBot = std::move(chatbot);
+  _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode) {
+  newNode->MoveChatbotHere(std::move(_chatBot));
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index) {
+  //// STUDENT CODE
+  ////
+
+  return _childEdges[index].get();
+
+  ////
+  //// EOF STUDENT CODE
+}
